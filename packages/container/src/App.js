cd6cfed021c931864896a60a4764d72759adcdc3
qtran46 <quangdoannhat.tran@dxc.com>
2024-01-02 15:08:41 +0700

    Added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import MarketingApp from './components/MarketingApp'
+import React, { lazy, Suspense, useState } from 'react'
 import Header from './components/Header'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 const generateClassName = createGenerateClassName({
   productionPrefix: 'con'
 })
-
+import Loading from './components/Loading'
 
 export default (props) => {
+  const [isSignedIn, setIsSignIn] = useState(false)
+  const onSignOut = () => {
+    setIsSignIn(false)
+  }
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header isSignedIn={isSignedIn} onSignOut={onSignOut} />
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              <Route path='/auth' >
+                <AuthLazy onSignIn={() => setIsSignIn(true)} />
+              </Route>
+              <Route path='/' component={MarketingLazy} />
+              <Route path='/dashboard' component={DashboardLazy} />
+
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
